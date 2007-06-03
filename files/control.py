b7c70e70b37495a2f939860714c2a36b0fc2dc7c
puexam <puexam@c573a8ba-ec2d-0410-ac71-a138ecf1ee33>
2007-06-03 15:27:59 +0000

    slownikowa czesc control.py prawie gotowa

--- files/control.py
+++ files/control.py
@@ -6,64 +6,115 @@ from charframe import *
 from dictionary import *
 from glob import glob
 class Control:
-    def __init__(self):
-        """class constructor; assigns atributes: self.options (list of options read from file) and self.dictionaries (list of available dictionaries)"""
-        optionFile=open('options.ini','r')
-        optionList=optionFile.readlines()
-        optionFile.close()
-        self.options=[opt[opt.index('=')+1:].strip() for opt in optionList]
-        self.dictionaries=[dic[-7:-4] for dic in glob('.\dict\*.txt')]
-    def saveOptions(self):
-        """saves atribute self.options to options.ini file"""
-        optionFile=open('options.ini','r')
-        optionList=optionFile.readlines()
-        optionFile.close()
-        optionFile=open('options.ini','w')
-        for i in range(len(optionList)):
-            optionList[i]=optionList[i][:optionList[i].index('=')+1]+str(self.options[i])+'\n'
-        print optionList
-        optionFile.writelines(optionList)
-        optionFile.close()
-    def inputFile(self,filename):
-        """returns binary read picture file"""
-        picture=open(filename,'rb')
-        return picture
-    def textRecognition(self,pic): # jeszcze bez wyjatkow
-        """main class method; uses methods below to cut picture file to lines and to characters, and to recognize characters"""
-        text=""
-        mainList=self.blockSegmentation(pic,[self.options[1],self.options[2]])
-        dictionaryObject=Dictionary(self.options[0])
-        for line in mainList:
-            for word in self.characterSegmentation(line,[]):
-                wordRead=[]
-                for char in word:
-                    wordRead.append(self.characterRecognition(char,[]))
-                text+=self.textComposition(wordRead,dictionaryObject)+' '
-            text+='\n'
-        return text
-    def blockSegmentation(self,picture,listOfOptions=[]):
-        """segmentation of a block of text into list of lines given as LineFrame objects"""
-        block=BlockFrame(picture)
-        block.blackWhite()
-        block.clear()
-        lines=block.extractLines()
-        return lines
-    def characterSegmentation(self,LineFrame,listOfOptions):
-        """segmentation of text line, returns list of lists of CharFrame objects """
-        characters = LineFrame.extractCharacters()
-        return characters
-    def characterRecognition (self,CharFrame,listOfOptions):
-        """recognition of one character given as an CharFrame object, returns a string"""
-        return [('a',23),('b',11),('c',8)]
-    def textComposition(self,tupleListList,dictionaryObject):
-        """method docstring"""
-        checkword=''
+	def __init__(self):
+		"""class constructor; assigns atributes: self.options (list of options read from file) and self.dictionaries (list of available dictionaries)"""
+		optionFile=open('options.ini','r')
+		optionList=optionFile.readlines()
+		optionFile.close()
+		self.options=[opt[opt.index('=')+1:].strip() for opt in optionList]
+		self.dictionaries=[dic[-7:-4] for dic in glob('.\dict\*.txt')]
+	def saveOptions(self):
+		"""saves atribute self.options to options.ini file"""
+		optionFile=open('options.ini','r')
+		optionList=optionFile.readlines()
+		optionFile.close()
+		optionFile=open('options.ini','w')
+		for i in range(len(optionList)):
+			optionList[i]=optionList[i][:optionList[i].index('=')+1]+str(self.options[i])+'\n'
+		print optionList
+		optionFile.writelines(optionList)
+		optionFile.close()
+	def inputFile(self,filename):
+		"""returns binary read picture file"""
+		picture=open(filename,'rb')
+		return picture
+	def textRecognition(self,pic): # jeszcze bez wyjatkow
+		"""main class method; uses methods below to cut picture file to lines and to characters, and to recognize characters"""
+		text=""
+		mainList=self.blockSegmentation(pic,[self.options[1],self.options[2]])
+		dictionaryObject=Dictionary(self.options[0])
+		for line in mainList:
+			for word in self.characterSegmentation(line,[]):
+				wordRead=[]
+				for char in word:
+					wordRead.append(self.characterRecognition(char,[]))
+				text+=self.textComposition(wordRead,dictionaryObject)+' '
+			text+='\n'
+		return text
+	def blockSegmentation(self,picture,listOfOptions=[]):
+		"""segmentation of a block of text into list of lines given as LineFrame objects"""
+		block=BlockFrame(picture)
+		block.blackWhite()
+		block.clear()
+		lines=block.extractLines()
+		return lines
+	def characterSegmentation(self,LineFrame,listOfOptions):
+		"""segmentation of text line, returns list of lists of CharFrame objects """
+		characters = LineFrame.extractCharacters()
+		return characters
+	def characterRecognition (self,CharFrame,listOfOptions):
+		"""recognition of one character given as an CharFrame object, returns a string"""
+		return [('a',23),('b',11),('c',8)]
+	def textComposition(self,tupleListList,dictionaryObject):
+		"""method docstring"""
+		checkword=''
 #        [[(a,23),(b,11),(c,8)],[(d,69),(e,44),(f,29)],[(g,96),(h,77),(i,63)]]
-        #buduje slowo z najlepszych i sprawdza je
-        for i in range(0,len(tupleListList)):
-            checkword=checkword+tupleListList[i][0][0]
+		
+		#buduje slowo z najlepszych i sprawdza je
+#		for i in range(0,len(tupleListList)):
+#			checkword=checkword+tupleListList[i][0][0]
 ##        if dictionaryObject.UniversalDictStruct.has(checkword)==True:
-        return checkword
+#		return checkword
+
+		ListListList=dictionaryObject.tupleListListToListListList(tupleListList)
+		word=dictionaryObject.normalize(tupleListList)
+		wordpos=dictionaryObject.bestPos(word)
+		
+		filenames=[]
+		palki=''
+		for i in range(wordpos[0],wordpos[1]-1):
+			palki=palki+'_'
+		filenames.append(word[wordpos[0]]+palki+word[wordpos[1]])
+		#plik=dictionaryObject.loadFiles(filenames)
+		plik=filenames[0]
+		start=wordpos[0]
+		wordpos.remove(wordpos[0])
+		wordpos.remove(wordpos[1])
+		
+		star=dictionaryObject.createDataStructure(plik)
+		permitted=dictionary.permittedWords(star,99-start+wordpos[0],word[wordpos[0]])
+		wordpos.remove(wordpos[0])
+		while True:
+			if wordpos==[]:
+				break
+			newpermitted=dictionary.permittedWords(star,99-start+wordpos[0],word[wordpos[0]])
+			if newpermitted==[]:
+				break
+			newnewpermitted==[]
+			for i in range(0,len(permitted)):
+				for j in range(0,len(newpermitted)):
+					if permitted[i]==newpermitted[j]:
+						newnewpermitted.append(permitted[i])
+			permitted=newnewpermitted
+			if permitted==[]:
+				break
+			wordpos.remove(wordpos[0])
+			
+		if wordpos==[]:#jest super
+			while True:
+				slownik=open(filename,'r')
+				a=-1
+				for linijka in slownik:
+					a+=1
+					if a==permitted[i]:
+						if len(word)==len(linijka)-2:
+							return linijka[2:]
+
+		if newpermitted==[]:
+			pass#tej literki w ogole nie ma w pliku na tej pozycji
+		if permitted==[]:
+			pass#ta literka jest w pliku ale nie pasuje do wczesniej wybranych
+
 ##        else:
 ##            #robi wspaniala strukture drzewiasta
 ##            word=dictionaryObject.normalize(tupleListList) #normalizuje
@@ -75,5 +126,6 @@ class Control:
 ##            return checkword
 
 if __name__ == "__main__":
-    a=Control()
-    a.saveOptions()
+	a=Control()
+	a.saveOptions()
+									
--- files/dictionary.py
+++ files/dictionary.py
@@ -1,102 +1,121 @@
 #-*- coding: utf8 -*-
 """Module docstring"""
 class Dictionary :
-        """class docstring"""
-        def __init__(self,filename) :
-                self.filename = filename 
-                pass
-        
-        def normalize (self, word) :
-                """method docstring"""
-                #potrzebuje listy listy list a nie listy listy krotek!
-                #[[(a,23),(b,11),(c,8)],[(d,69),(e,44),(f,29)],[(g,96),(h,77),(i,63)]]
-                for i in range(0,len(word)):
-                        suma=0
-                        for j in range(0,3):
-                                suma=suma+word[i][j][1]
-                        for j in range(0,3):
-                                word[i][j][1]=100*(word[i][j][1])/suma
-                return word
-                        
-        def loadFiles (self, filenames) :
-                """method docstring"""
-                pass
+	"""class docstring"""
+	def __init__(self,filename) :
+		self.filename = filename 
+		pass
+	
+	def tupleListListToListListList(tupleListList):
+		word=[]
+		for i in range(0,len(tupleListList)):
+			word.append([])
+			for j in range(0,len(tupleListList[i])):
+				word[i].append([tupleListList[i][j][0],tupleListList[i][j][1]])
+		return word
+	
+	def normalize (self, word) :
+		"""method docstring"""
+		
+		#[[(a,23),(b,11),(c,8)],[(d,69),(e,44),(f,29)],[(g,96),(h,77),(i,63)]]
+	
+		for i in range(0,len(word)):
+			suma=0
+			for j in range(0,3):
+				suma=suma+word[i][j][1]
+			for j in range(0,3):
+				word[i][j][1]=100*(word[i][j][1])/suma
+		return word
+			
+	def loadFiles (self, filenames) :
+		"""method docstring"""
+		filenames
+		pass
 
-        def bestPos (normalizedWord) :
-                """method docstring"""
-                #zwraca kolejnosc w jakiej literki sa prawdopodobne od najbardziej do najmniej
-                poszeregowane=[]
-                bestPosy=[]
-                for i in range(0,len(normalizedWord)):
-                        poszeregowane.append(normalizedWord[i][0][1])
-                for j in range(0,len(normalizedWord)):
-                        najlepszy=0
-                        najlepszymiejsce=0
-                        for i in range(0,len(normalizedWord)-j):
-                                if poszeregowane[i]>najlepszy:
-                                        najlepszy=poszeregowane[i]
-                                        najlepszymiejsce=i
-                        bestPosy.append(i)
-                        poszeregowane.remove(najlepszy)
-                bestPosy.reverse()
-                return bestPosy
+	def bestPos (normalizedWord) :
+		"""method docstring"""
+		#zwraca kolejnosc w jakiej literki sa prawdopodobne od najbardziej do najmniej
+		poszeregowane=[]
+		bestPosy=[]
+		for i in range(0,len(normalizedWord)):
+			poszeregowane.append(normalizedWord[i][0][1])
+		for j in range(0,len(normalizedWord)):
+			najlepszy=0
+			najlepszymiejsce=0
+			for i in range(0,len(normalizedWord)-j):
+				if poszeregowane[i]>najlepszy:
+					najlepszy=poszeregowane[i]
+					najlepszymiejsce=i
+			bestPosy.append(i)
+			poszeregowane.remove(najlepszy)
+		bestPosy.reverse()
+		return bestPosy
 
-        def createDataStructure (filename) :
-                """method docstring"""
-                struktura=([],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[])
-                slownik=open(filename,'r')
-                a=-1
-                for linijka in slownik:
-                        a+=1
-                        start=99-int(linijka[0])*10-int(linijka[1])
-                        for i in range(0,len(linijka)-3):
-                                jest=0
-                                for j in range(0,len(struktura[start+i])):
-                                        if struktura[start+i][j][0]==linijka[i+2]:
-                                                struktura[start+i][j][1].append(a)
-                                                jest=1
-                                if jest==0:
-                                        struktura[start+i].append((linijka[i+2],[a]))
-                                        jest=0
-                slownik.close()
-                return struktura
+	def createDataStructure (filename) :
+		"""method docstring"""
+		struktura=([],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[])
+		slownik=open(filename,'r')
+		a=-1
+		for linijka in slownik:
+			a+=1
+			start=99-int(linijka[0])*10-int(linijka[1])
+			for i in range(0,len(linijka)-3):
+				jest=0
+				for j in range(0,len(struktura[start+i])):
+					if struktura[start+i][j][0]==linijka[i+2]:
+						struktura[start+i][j][1].append(a)
+						jest=1
+				if jest==0:
+					struktura[start+i].append((linijka[i+2],[a]))
+					jest=0
+		slownik.close()
+		return struktura
 
-        def has (self, word) :
-                """method docstring"""
-                pass
+	def permittedWords (struktura,miejsce,litera) :
+		"""method docstring"""
+		for i in range(0,len(struktura[miejsce])):
+			if struktura[miejsce][i][0]==litera:
+				j=i
+		wyrazy=struktura[miejsce][j][1]
+		wyrazy.sort()
+		return wyrazy
 
-class StarStruct :
-        """class docstring"""
-        def __init__(self) :
-                pass
-        
-        def get (self) :
-                """method docstring"""
-                pass
+	def has (self, word) :
+		"""method docstring"""
+		pass
 
-        def has (self, word) :
-                """method docstring"""
-                pass
-
-        def size (self) :
-                """method docstring"""
-                pass
-
-class UniversalDictStruct : #w ogole nie wiem jak to zrobic ;f w ogole nie wiem czy to potrzebne tak na prawde
-        """class docstring"""
-        def __init__(self,data) :
-                self.data=data
-                self.children=[]
-
-        def insert (self, word) :
-                """method docstring"""
-                for i in range(1,len(word)):
-                        self.children.append(word[i])
-                
-        def has (self, word) :
-                """method docstring"""
-                pass
+##class StarStruct :
+##        """class docstring"""
+##        def __init__(self) :
+##                pass
+##        
+##        def get (self) :
+##                """method docstring"""
+##                pass
+##
+##        def has (self, word) :
+##                """method docstring"""
+##                pass
+##
+##        def size (self) :
+##                """method docstring"""
+##                pass
+##
+##class UniversalDictStruct : #w ogole nie wiem jak to zrobic ;f w ogole nie wiem czy to potrzebne tak na prawde
+##        """class docstring"""
+##        def __init__(self,data) :
+##                self.data=data
+##                self.children=[]
+##
+##        def insert (self, word) :
+##                """method docstring"""
+##                for i in range(1,len(word)):
+##                        self.children.append(word[i])
+##                
+##        def has (self, word) :
+##                """method docstring"""
+##                pass
 
 if __name__ == "__main__": #this runs, when code is running as an own program, not as a module
-        #you can use this section to test your module
-        pass
+	#you can use this section to test your module
+	pass
